6acf6c497e80593c9740b76fa2cc06fcb41d3909
masaru-suzuki <masaru.suzuki.yamagata@gmail.com>
2024-08-13 16:03:48 +0900

    mod:お気に入り店舗画面のコンテンツを作成

--- src/app/(spa)/end-users/[userId]/favoriteShop/[favoriteShopId]/page.tsx
+++ src/app/(spa)/end-users/[userId]/favoriteShop/[favoriteShopId]/page.tsx
@@ -1,9 +1,9 @@
 'use client';
 
 import { PageHeader } from '@/components/PageHeader';
+import { favoriteShopList } from '@/constants/favoriteShop';
 import { userList } from '@/constants/user';
 import { generateBreadCrumbs } from '@/utils/GenerateBreadCrumbs';
-import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 
 type Params = {
@@ -17,14 +17,27 @@ export default function Page({ params }: Params) {
   const pathname = usePathname();
   const breadcrumbs = generateBreadCrumbs({ pathname, params });
   const userName = userList.find((user) => user.userId === userId)?.name;
+  const shopName = favoriteShopList.find(
+    (shop) => shop.favoriteShopId === favoriteShopId
+  )?.label;
 
   return (
     <>
       <PageHeader breadcrumbs={breadcrumbs} />
       <div className="p-2 grid gap-8 content-start">
-        <Link href={`/end-users/${userId}`}>
-          ユーザー: {userName} の管理画面
-        </Link>
+        <div>
+          ユーザー: {userName} のお気に入り店舗 （{shopName}）の詳細コンテンツ
+        </div>
+        <dl className="flex gap-4">
+          <dt>電話番号:</dt>
+          <dd>
+            <a href="tel:+">000-0000-0000</a>
+          </dd>
+        </dl>
+        <dl className="flex gap-4">
+          <dt>住所:</dt>
+          <dd>仙台市青葉区国分町1丁目1-1</dd>
+        </dl>
       </div>
     </>
   );
