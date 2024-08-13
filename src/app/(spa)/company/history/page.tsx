f29520e289ee1adcc181fd12954ca8652a4b62ac
masaru-suzuki <masaru.suzuki.yamagata@gmail.com>
2024-08-13 13:17:34 +0900

    add:static pathのページを作成

---
+++ src/app/(spa)/company/history/page.tsx
@@ -0,0 +1,34 @@
+'use client';
+
+import { PageHeader } from '@/components/PageHeader';
+import { generateBreadCrumbs } from '@/utils/GenerateBreadCrumbs';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
+export default function Page() {
+  // const pathname = usePathname();
+  // const breadcrumbs = generateBreadCrumbs({ pathname });
+  const breadcrumbs = [
+    {
+      label: '企業情報',
+      href: '/company',
+    },
+    {
+      label: '沿革',
+      href: '/company/history',
+    },
+  ];
+
+  return (
+    <>
+      <PageHeader breadcrumbs={breadcrumbs} />
+      <div className="p-2 grid gap-8 content-start">
+        <div>沿革ページコンテンツ</div>
+        <div className="grid gap-2">
+          <Link href="/company/officers">役員</Link>
+          <Link href="/company">企業TOP</Link>
+        </div>
+      </div>
+    </>
+  );
+}
