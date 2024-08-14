2e12aea00eff00c22291c2a2bc97e2af7516330d
masaru-suzuki <masaru.suzuki.yamagata@gmail.com>
2024-08-14 16:50:18 +0900

    clean:useBreadcrumbsにts-docを追加

--- src/hooks/useBreadcrumbs.ts
+++ src/hooks/useBreadcrumbs.ts
@@ -7,6 +7,16 @@ type UseBreadcrumbsArg = {
   params?: Record<string, string>;
   customBreadcrumbs?: Breadcrumb[];
 };
+
+/**
+ * useBreadcrumbs カスタムフック
+ *
+ * このフックは、現在のURLパスとオプションのカスタムパンくずリストを使用して
+ * パンくずリストを生成し、BreadcrumbsContextに設定します。
+ *
+ * @param params - URLパスに含まれる動的ルートパラメータを含むオブジェクト (例: { userId: "1" })
+ * @param customBreadcrumbs - カスタムパンくずリストを手動で指定する場合に使用。指定がない場合は自動生成される。
+ */
 export const useBreadcrumbs = ({
   params,
   customBreadcrumbs,
